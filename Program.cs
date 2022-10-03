65ed0f3f8a92bed92fef360ab57c70757908819e
rita <ritavlas@yandex.ru>
2022-10-03 10:16:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Time
+{
+    internal class Program
+    {              
+       static void PrintTime(object temp)
+       {
+         Console.Clear();
+         Console.WriteLine("Сurrent time: " + DateTime.Now.ToLongTimeString());
+       }
+        static void Main()
+        {
+         TimerCallback timeCB = new TimerCallback(PrintTime);
+         Timer time = new Timer(timeCB, null, 0, 1000);
+         Console.ReadLine();
+        }
+        
+    }
+}
+    
